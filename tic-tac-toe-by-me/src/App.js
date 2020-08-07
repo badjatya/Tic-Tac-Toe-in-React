187e27dca373e0e3c201d8b9526a820cc9cf94f9
badjatya <architj240@gmail.com>
2020-08-08 05:02:20 +0530

    second

--- tic-tac-toe-by-me/src/App.js
+++ tic-tac-toe-by-me/src/App.js
@@ -1,34 +1,36 @@
-import React, { useState } from "react";
+import React ,{useState} from 'react';
 import logo from "./logo.svg";
-import Icon from "./components/Icon";
 
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
-import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
-import "bootstrap/dist/css/bootstrap.css";
-import "./App.css";
+import Icon from "./components/Icon"
 
-const itemArray = new Array(9).fill("empty");
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import {Card,CardBody,Container,Button,Row,Col} from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+const itemArray = new Array(9).fill("empty")
 
 const App = () => {
-  const [isCross, setIsCross] = useState(false);
-  const [winMessage, setWinMessage] = useState("");
+
+
+  const [isCross,setIsCross] = useState(false)
+  const [winMessage,setWinMessage] = useState("")
+
 
   const reloadGame = () => {
-    setIsCross(false);
-    setWinMessage("");
-    itemArray.fill("empty", 0, 9);
-  };
+    setIsCross(false)
+    setWinMessage("")
+    itemArray.fill("empty",0,9)
+  }
+
 
   const checkIsWinner = () => {
-    //  checking  winner of the game
-    if (
-      itemArray[0] === itemArray[1] &&
-      itemArray[0] === itemArray[2] &&
-      itemArray[0] !== "empty"
-    ) {
-      setWinMessage(`${itemArray[0]} won`);
+
+    if (itemArray[0] !== "empty" && itemArray[0] === itemArray[1] && itemArray[0] === itemArray[3] ) {
+      setWinMessage(`${itemArray[0]} Wins`)
     } else if (
       itemArray[3] !== "empty" &&
       itemArray[3] === itemArray[4] &&
@@ -72,55 +74,63 @@ const App = () => {
     ) {
       setWinMessage(`${itemArray[2]} won`);
     }
-  };
+
+    
+  }
+
 
   const changeItem = itemNumber => {
     if (winMessage) {
-      return toast(winMessage, { type: "success" });
+      return toast(winMessage,{type: "success"})
     }
 
     if (itemArray[itemNumber] === "empty") {
-      itemArray[itemNumber] = isCross ? "cross" : "circle";
+      itemArray[itemNumber] = isCross ?"cross" :"circle";
       setIsCross(!isCross);
-    } else {
-      return toast("already filled", { type: "error" });
+    }else{
+      return toast("Already Filled",{type:"error"})
     }
+    checkIsWinner()
+  }
 
-    checkIsWinner();
-  };
 
   return (
-    <Container className="p-5">
+    
+    <Container className ="p-5">
+
       <ToastContainer position="bottom-center" />
+
       <Row>
         <Col md={6} className="offset-md-3">
-          {winMessage ? (
-            <div className="mb-2 mt-2">
-              <h1 className="text-success text-uppercase text-center">
-                {winMessage}
-              </h1>
-              <Button color="success" block onClick={reloadGame}>
-                Reload the game
-              </Button>
-            </div>
-          ) : (
-            <h1 className="text-center text-warning">
-              {isCross ? "Cross" : "Circle"} turns
+        <h1 id="heading" className="text-center ">Tic Tac Toe</h1>
+        {winMessage ? (
+          <div className = "mb-2 mt-2">
+            <h1 className = "text-success text-center text-uppercase">
+              {winMessage}
             </h1>
-          )}
+            <Button color = "success" block onClick ={reloadGame}>Reaload The Game</Button>
+          </div>
+        ) : (
+          <h1 className = "text-center text-warning ">
+            {isCross ? "Cross" : "Circle"} turn
+          </h1>
+        )}
           <div className="grid">
-            {itemArray.map((item, index) => (
-              <Card color="warning" onClick={() => changeItem(index)}>
+            {itemArray.map((item , index) => (
+              <Card color = "success" onClick={() => changeItem(index)}>
                 <CardBody className="box">
-                  <Icon name={item} />
+                  <Icon name = {item} />
                 </CardBody>
               </Card>
-            ))}
+             ))}
           </div>
         </Col>
       </Row>
-    </Container>
-  );
-};
 
-export default App;
+    </Container>
+
+  )
+}
+
+
+export default App
